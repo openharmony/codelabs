cc7459d2483c27c36d4d04b9d25226fda553064e
1 <13343468815@163.com>
2022-04-01 17:20:17 +0800

    330适配

--- Media/Audio_OH_ETS/hvigorfile.js
+++ Media/Audio_OH_ETS/hvigorfile.js
@@ -1,2 +1,2 @@
 // Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
-module.exports = require('@ohos/hvigor-ohos-plugin').appTasks
+module.exports = require('@ohos/hvigor-ohos-plugin').legacyAppTasks
