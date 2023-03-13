bf9766b6fe18ae90ec3da8b57904f614408c4cba
xlleng110@163.com <xlleng110@163.com>
2023-03-13 20:21:23 +0800

    最小化适配

--- ETSUI/Healthy_life/entry/src/main/ets/entryability/EntryAbility.ts
+++ ETSUI/Healthy_life/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,59 +1,62 @@
-import hilog from '@ohos.hilog';
-import Ability from '@ohos.application.Ability'
-import Window from '@ohos.window'
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License,Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-export default class EntryAbility extends Ability {
+import UIAbility from '@ohos.app.ability.UIAbility';
+
+export default class MainAbility extends UIAbility {
+    private static TAG: string = 'MainAbility';
+
     onCreate(want, launchParam) {
-        hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
-        hilog.info(0x0000, 'testTag', '%{public}s', 'want param:' + JSON.stringify(want) ?? '');
-        hilog.info(0x0000, 'testTag', '%{public}s', 'launchParam:' + JSON.stringify(launchParam) ?? '');
+        console.debug(MainAbility.TAG, 'onCreate');
         globalThis.abilityWant = want;
-        globalThis.abilityContext = this.context;
     }
 
     onDestroy() {
-        hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
+        console.debug(MainAbility.TAG, 'onDestroy');
     }
 
-    onWindowStageCreate(windowStage: Window.WindowStage) {
+    onWindowStageCreate(windowStage) {
         // Main window is created, set main page for this ability
-        hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
-
-        windowStage.loadContent('pages/SplashPage', (err, data) => {
+        console.debug(MainAbility.TAG, 'onWindowStageCreate');
+        windowStage.loadContent("pages/SplashPage", (err, data) => {
             if (err.code) {
-                hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.ERROR);
-                hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+                console.error('windowStage', 'Failed to load the content. Cause:' + JSON.stringify(err));
                 return;
             }
-            hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
-            hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+            console.info('windowStage', 'Succeeded in loading the content. Data: ' + JSON.stringify(data));
         });
     }
 
     onWindowStageDestroy() {
         // Main window is destroyed, release UI related resources
-        hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
+        console.debug(MainAbility.TAG, 'onWindowStageDestroy');
     }
 
     onForeground() {
         // Ability has brought to foreground
-        hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
+        console.debug(MainAbility.TAG, 'onForeground');
     }
 
     onBackground() {
         // Ability has back to background
-        hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+        console.debug(MainAbility.TAG, 'onBackground');
     }
 
     onNewWant(want, launchParam) {
         // Ability has new want
-        console.debug('testTag', `onNewWant ${JSON.stringify(want)}`);
+        console.debug(MainAbility.TAG, `onNewWant ${JSON.stringify(want)}`);
         globalThis.abilityWant = want;
     }
-}
+};
