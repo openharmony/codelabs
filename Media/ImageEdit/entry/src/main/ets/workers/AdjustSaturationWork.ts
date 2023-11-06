7a01aef364c0a6b3526c7b53e1df262bac55fdc4
13871184879 <452399386@qq.com>
2023-11-06 11:16:27 +0800

    图片编辑（ArkTS）Codelinter整改
    Signed-off-by: 13871184879 <452399386@qq.com>

--- Media/ImageEdit/entry/src/main/ets/workers/AdjustSaturationWork.ts
+++ Media/ImageEdit/entry/src/main/ets/workers/AdjustSaturationWork.ts
@@ -17,39 +17,39 @@ import hilog from '@ohos.hilog';
 import worker, { ThreadWorkerGlobalScope, MessageEvents, ErrorEvent } from '@ohos.worker';
 import { adjustSaturation } from '../utils/AdjustUtil';
 
-let workerPort : ThreadWorkerGlobalScope = worker.workerPort;
+let workerPort: ThreadWorkerGlobalScope = worker.workerPort;
 
 /**
-* Defines the event handler to be called when the worker thread receives a message sent by the host thread.
-* The event handler is executed in the worker thread.
-*
-* @param e message data
-*/
-workerPort.onmessage = function(event : MessageEvents) {
-    let bufferArray = event.data.buf;
-    let last = event.data.last;
-    let cur = event.data.cur;
-    let buffer = adjustSaturation(bufferArray, last, cur)
-    workerPort.postMessage(buffer);
-    workerPort.close();
+ * Defines the event handler to be called when the worker thread receives a message sent by the host thread.
+ * The event handler is executed in the worker thread.
+ *
+ * @param e message data
+ */
+workerPort.onmessage = function (event: MessageEvents) {
+  let bufferArray = event.data.buf;
+  let last = event.data.last;
+  let cur = event.data.cur;
+  let buffer = adjustSaturation(bufferArray, last, cur)
+  workerPort.postMessage(buffer);
+  workerPort.close();
 }
 
 /**
-* Defines the event handler to be called when the worker receives a message that cannot be deserialized.
-* The event handler is executed in the worker thread.
-*
-* @param e message data
-*/
-workerPort.onmessageerror = function(event : MessageEvents) {
-    hilog.error(0x0000, 'AdjustSaturationWork', 'Failed to load the content. Cause: %{public}s', `on message error ${JSON.stringify(event)}`);
+ * Defines the event handler to be called when the worker receives a message that cannot be deserialized.
+ * The event handler is executed in the worker thread.
+ *
+ * @param e message data
+ */
+workerPort.onmessageerror = function (event: MessageEvents) {
+  hilog.error(0x0000, 'AdjustSaturationWork', 'Failed to load the content. Cause: %{public}s', `on message error ${JSON.stringify(event)}`);
 }
 
 /**
-* Defines the event handler to be called when an exception occurs during worker execution.
-* The event handler is executed in the worker thread.
-*
-* @param e error message
-*/
-workerPort.onerror = function(error : ErrorEvent) {
-    hilog.error(0x0000, 'AdjustSaturationWork', 'Failed to load the content. Cause: %{public}s', `on worker error ${JSON.stringify(error)}`);
+ * Defines the event handler to be called when an exception occurs during worker execution.
+ * The event handler is executed in the worker thread.
+ *
+ * @param e error message
+ */
+workerPort.onerror = function (error: ErrorEvent) {
+  hilog.error(0x0000, 'AdjustSaturationWork', 'Failed to load the content. Cause: %{public}s', `on worker error ${JSON.stringify(error)}`);
 }
