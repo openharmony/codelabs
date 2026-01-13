67e1cf8eee4860fb793c47ed17f8ea840d9ce9c7
superchao1 <3420240042@bit.edu.cn>
2026-01-13 10:24:40 +0800

    format code,Signed-off-by: superchao1 <3420240042@bit.edu.cn>

--- NativeAPI/FunctionFlowRuntime/entry/src/main/cpp/types/libentry/Index.d.ts
+++ NativeAPI/FunctionFlowRuntime/entry/src/main/cpp/types/libentry/Index.d.ts
@@ -1,9 +1,13 @@
 export class FunctionClass {
   constructor(version: number)
-  FfrtSerialQueue():number;
-  FfrtConcurrentQueue():number;
+
+  FfrtSerialQueue(): number;
+
+  FfrtConcurrentQueue(): number;
 }
 
 export const add: (a: number, b: number) => number;
+
 export const ProcessFfrtQueue: () => number;
+
 export const ComputeFfrtQueue: () => number;
