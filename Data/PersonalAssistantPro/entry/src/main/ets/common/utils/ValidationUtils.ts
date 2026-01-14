7915305e86e7f624a970fe3a13be767f2028aa14
yangkunlong <3220251406@bit.edu.cn>
2026-01-14 20:47:37 +0800

    Fix(PersonalAssistantPro): resolve static check errors (OAT.1/3/4)
    Reason:
    Fix static analysis errors reported during PR checks.
    
    Changes:
    1. Add missing Apache License 2.0 headers (OAT.3) and Copyright headers (OAT.4) to all source files.
    2. Fix 'Invalid File Type: binary' errors (OAT.1) by ensuring UTF-8 encoding for debug cases and resource utils.
    3. Update Copyright year to 2025.
    
    Signed-off-by: yangkunlong <3220251406@bit.edu.cn>

--- Data/PersonalAssistantPro/entry/src/main/ets/common/utils/ValidationUtils.ts
+++ Data/PersonalAssistantPro/entry/src/main/ets/common/utils/ValidationUtils.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * 正则校验工具类
  * 用于表单验证：手机号、邮箱、身份证、密码强度等
