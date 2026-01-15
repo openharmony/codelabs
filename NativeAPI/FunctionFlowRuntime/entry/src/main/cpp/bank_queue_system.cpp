f5f1d5bf7656564e269d2ff07b97181c8b308241
superchao1 <3420240042@bit.edu.cn>
2026-01-15 15:08:04 +0800

    fix code,Signed-off-by: superchao1 <3420240042@bit.edu.cn>

--- NativeAPI/FunctionFlowRuntime/entry/src/main/cpp/bank_queue_system.cpp
+++ NativeAPI/FunctionFlowRuntime/entry/src/main/cpp/bank_queue_system.cpp
@@ -1,34 +1,52 @@
-
+/*
+ * Copyright (c) 2025 Beijing Institude of Technology.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "bank_queue_system.h"
 #include "native_log_wrapper.h"
 
-BankQueueSystem::BankQueueSystem(int type, const char *name, int concurrency) {
+BankQueueSystem::BankQueueSystem(int type, const char *name, int concurrency) 
+{
     if (type == 0) {
-        queue_ =
-            std::make_unique<ffrt::queue>(ffrt::queue_concurrent, name, ffrt::queue_attr().max_concurrency(concurrency));
+        queue_ = std::make_unique<ffrt::queue>(
+            ffrt::queue_concurrent, name, ffrt::queue_attr().max_concurrency(concurrency));
     } else {
-        queue_ =
-            std::make_unique<ffrt::queue>(ffrt::queue_serial, name, ffrt::queue_attr().max_concurrency(concurrency));
+        queue_ = std::make_unique<ffrt::queue>(
+            ffrt::queue_serial, name, ffrt::queue_attr().max_concurrency(concurrency));
     }
     LOGI("bank system has been initialized");
 }
 
-BankQueueSystem::~BankQueueSystem() {
+BankQueueSystem::~BankQueueSystem()
+{
     queue_ = nullptr;
     LOGI("bank system has been destroyed");
 }
 
 // 开始排队，即提交队列任务
-ffrt::task_handle BankQueueSystem::Enter(const std::function<void()> &func, const char *name, ffrt_queue_priority_t level, int delay) {
+ffrt::task_handle BankQueueSystem::Enter(const std::function<void()> &func, const char *name, ffrt_queue_priority_t level, int delay)
+{
     return queue_->submit_h(func, ffrt::task_attr().name(name).priority(level).delay(delay));
 }
 
 // 退出排队，即取消队列任务
-int BankQueueSystem::Exit(const ffrt::task_handle &t) {
+int BankQueueSystem::Exit(const ffrt::task_handle &t)
+{
     return queue_->cancel(t); 
 }
 
 // 等待排队，即等待队列任务
-void BankQueueSystem::Wait(const ffrt::task_handle &handle) {
+void BankQueueSystem::Wait(const ffrt::task_handle &handle) 
+{
     queue_->wait(handle); 
 }
--- NativeAPI/FunctionFlowRuntime/entry/src/main/cpp/function_class_cpp.h
+++ NativeAPI/FunctionFlowRuntime/entry/src/main/cpp/function_class_cpp.h
@@ -1,4 +1,17 @@
-
+/*
+ * Copyright (c) 2025 Beijing Institude of Technology.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef OHOS_SIMPLE_FUNCTIONCLASS_H
 #define OHOS_SIMPLE_FUNCTIONCLASS_H
 
@@ -8,7 +21,7 @@
 class FunctionClass {
 public:
     FunctionClass() = default;
-    FunctionClass(uint32_t version);
+    explicit FunctionClass(uint32_t version);
     ~FunctionClass();
     int FfrtSerialQueue();
     int FfrtConcurrentQueue();
