4b3006bd03d1a9d153ba0c898fb2df2d1941c75c
superchao1 <3420240042@bit.edu.cn>
2026-01-20 11:48:44 +0800

    fix code,Signed-off-by: superchao1 <3420240042@bit.edu.cn>

--- NativeAPI/FunctionFlowRuntime/entry/src/main/cpp/sort_class.h
+++ NativeAPI/FunctionFlowRuntime/entry/src/main/cpp/sort_class.h
@@ -1,8 +1,17 @@
-//
-// Created on 2026/1/13.
-//
-// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
-// please include "napi/native_api.h".
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef FUNCTIONFLOWRUNTIME_SORT_CLASS_H
 #define FUNCTIONFLOWRUNTIME_SORT_CLASS_H
