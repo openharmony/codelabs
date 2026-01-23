851f53902c47e62a28a1a50ac6f3795e6c46a3df
林玉 <2552789747@qq.com>
2026-01-24 00:04:36 +0800

    fix: 精准修正 PreferenceUtil.ts 版权头（2026 Huawei Device Co., Ltd.）
    Signed-off-by: 林玉 <2552789747@qq.com>

--- FamilyMedicineBox/entry/src/main/ets/utils/PreferenceUtil.ts
+++ FamilyMedicineBox/entry/src/main/ets/utils/PreferenceUtil.ts
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 2025 Beijing Institute of Technology.
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSEt-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,9 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// 下面是你的业务代码（比如 import 语句、类定义等）
 
 
-    } catch (err) {
+
+} catch (err) {
       // 【修复】严格模式下，将错误转换为字符串打印
       console.error('Preferences load failed', JSON.stringify(err));
     }
