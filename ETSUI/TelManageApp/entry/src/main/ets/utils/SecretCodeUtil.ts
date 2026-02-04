45acad60a3e979d3037d8a678e8df03668d5c795
jyhan_cs <3220251471@bit.edu.cn>
2026-02-04 16:54:57 +0800

    add
    Signed-off-by: jyhan_cs <3220251471@bit.edu.cn>

--- ETSUI/TelManageApp/entry/src/main/ets/utils/SecretCodeUtil.ts
+++ ETSUI/TelManageApp/entry/src/main/ets/utils/SecretCodeUtil.ts
@@ -1,4 +1,16 @@
 /*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-20
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
  * 模块功能：密令编码与解码工具。
  * 作用：将联系人字段压缩并混淆成短密令，支持解析还原。
  * 输出：密令字符串或联系人字段对象。
